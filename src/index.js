3eda056ebc04a2af12d959624724b3da1ea76769
Jillo Wario <willywario0@gmail.com>
2024-01-11 19:46:04 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (e) => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  const list = document.getElementById("tasks");
+  const input = document.getElementById("new-task-description");
+
+  let tasks = [];
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    tasks.push(input.value);
+    input.value = null;
+    const li = document.createElement("li");
+    tasks.map((task) => {
+      li.textContent = task;
+      list.appendChild(li);
+    });
+  });
 });
